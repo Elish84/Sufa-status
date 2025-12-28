a93f6a01269b505e9222e76da300f7c85b843e32
Elish84 <89509077+Elish84@users.noreply.github.com>
2025-12-28 09:57:47 +0200

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// firebase-config.js
+window.firebaseConfig = {
+  apiKey: "AIzaSyBtf_w7snO_7hyJhv_Z_eFbgzh43FmR2SU",
+  authDomain: "sufa-73c50.firebaseapp.com",
+  projectId: "sufa-73c50",
+  storageBucket: "sufa-73c50.firebasestorage.app",
+  messagingSenderId: "627835144004",
+  appId: "1:627835144004:web:6ea1c1370f4ca37b5d7911",
+  measurementId: "G-NLYG09HGLQ"
+};
